1aa3f0073850d36fbdff1e4ef02b5334916b3bbc
testgithubanjali <131359262+testgithubanjali@users.noreply.github.com>
2026-04-01 13:23:04 +0530

    add: added code in dockerfile and dockecompose and env file and updated code accordingly

--- config/db.go
+++ config/db.go
@@ -2,7 +2,9 @@ package config
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
+	"os"
 
 	_ "github.com/lib/pq"
 )
@@ -10,17 +12,38 @@ import (
 var DB *sql.DB
 
 func ConnectDB() {
-	connStr := "user=postgres password=yourpassword dbname=finance_db sslmode=disable"
+	// Read from environment variables (Docker will pass these)
+	host := getEnv("DB_HOST", "localhost")
+	user := getEnv("DB_USER", "postgres")
+	password := getEnv("DB_PASSWORD", "postgres")
+	dbname := getEnv("DB_NAME", "finance_db")
+	port := getEnv("DB_PORT", "5432")
+
+	connStr := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+		host, user, password, dbname, port,
+	)
+
+	log.Println("Connecting to DB...")
 
 	db, err := sql.Open("postgres", connStr)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal("DB open error:", err)
 	}
 
 	if err := db.Ping(); err != nil {
-		log.Fatal(err)
+		log.Fatal("DB ping error:", err)
 	}
 
 	DB = db
-	log.Println("✅ DB Connected")
+	log.Println("✅ Connected to PostgreSQL")
+}
+
+// helper function
+func getEnv(key, fallback string) string {
+	value := os.Getenv(key)
+	if value == "" {
+		return fallback
+	}
+	return value
 }
--- main.go
+++ main.go
@@ -3,16 +3,20 @@ package main
 import (
 	"finance-backend/config"
 	"finance-backend/routes"
+	"log"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	log.Println("🚀 Starting Finance Backend...")
+
 	config.ConnectDB()
 
 	r := gin.Default()
 
 	routes.SetupRoutes(r)
 
+	log.Println("🌐 Server running on port 8080")
 	r.Run(":8080")
 }
