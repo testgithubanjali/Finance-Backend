da92db1af5aab1ad6cec3bb7a97255b97985489a
testgithubanjali <131359262+testgithubanjali@users.noreply.github.com>
2026-04-01 13:11:36 +0530

    add: added code in middleware

---
+++ utils/jwt.go
@@ -0,0 +1,28 @@
+package utils
+
+import "github.com/golang-jwt/jwt/v5"
+
+var SECRET = []byte("secret")
+
+func GenerateToken(userID, role string) string {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"user_id": userID,
+		"role":    role,
+	})
+
+	tokenStr, _ := token.SignedString(SECRET)
+	return tokenStr
+}
+
+func ValidateToken(tokenStr string) (string, string, error) {
+	token, err := jwt.Parse(tokenStr, func(t *jwt.Token) (interface{}, error) {
+		return SECRET, nil
+	})
+
+	if err != nil {
+		return "", "", err
+	}
+
+	claims := token.Claims.(jwt.MapClaims)
+	return claims["user_id"].(string), claims["role"].(string), nil
+}
